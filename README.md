f7c7b99d9a4d953b713b8fdbeab4625727076a96
Shrivastava <srajatshrivastava@gmail.com>
2024-07-08 13:56:41 +0200

    add new question and roadmap diagram

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # neet_leet_code
 Fun Coding
+
+Focus on these topics in the order:
+
+** Arrays, Decision Trees, HashMaps **
+** Graphs, Heaps **
+** Dynamic Programming ** : Focus only on famous problems for this one or company specific problems
+
+Check DSA Roadmap for proper walkthrough fundamentals and then proceed. (Source Credit: Neetcode.io)
--- easy/1_valid_anagram.py
+++ easy/1_valid_anagram.py
@@ -1,1 +1,34 @@
+"""
+Given two strings s and t, return true if t is an anagram of s and false otherwise.
+"""
 
+s = input("Enter string s: ")
+t = input("Enter string t: ")
+
+
+def check_anagram(s: str, t: str) -> bool:
+    if len(s) == len(t):
+        if set(s) == set(t):
+            return check_anagram_2(s, t)
+        return False
+    return False
+
+
+def check_anagram_2(s: str, t: str) -> bool:
+    # convert string to list to use inbuilt sort method of lists
+    # there is another option to use sorted(): sorted() function accepts any iterable and not just lists
+    s_list = list(s)
+    t_list = list(t)
+    sorted_s = s_list.sort()
+    sorted_t = t_list.sort()
+    if sorted_s == sorted_t:
+        return True
+    return False
+
+
+value = check_anagram(s, t)
+
+if value:
+    print("Strings are anagram")
+else:
+    print("Not anagram")
